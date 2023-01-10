62dda14d0d812eb7c4717fea7ed4075ab071992f
Mukul Vishala <119170857+mukullvishala@users.noreply.github.com>
2023-01-10 18:33:18 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+function f1() {
+
+  let txt = document.getElementById('txt').value
+
+
+  if (txt == "mukulchaudhary") {
+    alert("Login Sucessful")
+  }
+  else {
+
+    alert("Wrong Password")
+
+  }
+
+
+
+
+
+
+
+
+
+
+}
